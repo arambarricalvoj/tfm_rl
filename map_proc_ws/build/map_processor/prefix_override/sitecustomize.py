2b861352ff5c13a10aaabadc3d788161ec3d0a62
Javier Arambarri Calvo <javier.arambarri@ehu.eus>
2026-04-27 09:38:22 +0200

    entrenamiento baseline

--- map_proc_ws/build/map_processor/prefix_override/sitecustomize.py
+++ map_proc_ws/build/map_processor/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/javierac/map_proc_ws/install/map_processor'
+    sys.prefix = sys.exec_prefix = '/home/isaac_sim/map_proc_ws/install/map_processor'
